a7f72a6062210b4f9cb712f000cbc98ed7fa02bf
Odonno <bottiau.david@laposte.net>
2023-04-10 18:57:45 +0200

    ♻️ create function create_surrealdb_client

--- src/list.rs
+++ src/list.rs
@@ -2,9 +2,8 @@ use chrono::{DateTime, Utc};
 use chrono_human_duration::ChronoHumanDuration;
 use cli_table::{format::Border, Cell, ColorChoice, Style, Table};
 use std::process;
-use surrealdb::{engine::remote::ws::Ws, opt::auth::Root, Surreal};
 
-use crate::{config, models::ScriptMigration};
+use crate::{models::ScriptMigration, surrealdb};
 
 pub async fn main(
     url: Option<String>,
@@ -14,38 +13,7 @@ pub async fn main(
     password: Option<String>,
     no_color: bool,
 ) {
-    let db_config = config::retrieve_db_config();
-
-    let url = url.or(db_config.url).unwrap_or("localhost:8000".to_owned());
-
-    let connection = Surreal::new::<Ws>(url.to_owned()).await;
-
-    if let Err(error) = connection {
-        eprintln!("{}", error);
-        process::exit(1);
-    }
-
-    let client = connection.unwrap();
-
-    let username = username.or(db_config.username).unwrap_or("root".to_owned());
-    let password = password.or(db_config.password).unwrap_or("root".to_owned());
-
-    client
-        .signin(Root {
-            username: &username,
-            password: &password,
-        })
-        .await
-        .unwrap();
-
-    let ns = ns.or(db_config.ns).unwrap_or("test".to_owned());
-    let db = db.or(db_config.db).unwrap_or("test".to_owned());
-
-    client
-        .use_ns(ns.to_owned())
-        .use_db(db.to_owned())
-        .await
-        .unwrap();
+    let client = surrealdb::create_surrealdb_client(url, ns, db, username, password).await;
 
     let response = client.select("script_migration").await;
 
