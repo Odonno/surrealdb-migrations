8f30b7142eb7cf84b34d49eafd1bd190337e9c84
Odonno <bottiau.david@laposte.net>
2023-05-13 19:08:10 +0200

    ✅ use range to avoid random error on list_blog_migrations test

--- tests/library/list.rs
+++ tests/library/list.rs
@@ -43,36 +43,41 @@ async fn list_blog_migrations() -> Result<()> {
 
             let date_prefix = now.format("%Y%m%d_%H%M").to_string();
 
+            let now_timestamp = now.timestamp();
+            let now_timestamp_range = (now_timestamp - 2)..(now_timestamp + 2);
+
             let first_migration = migrations_applied
                 .get(0)
                 .context("Cannot get first migration")?;
 
             ensure!(first_migration.script_name == format!("{}01_AddAdminUser", date_prefix));
-            ensure!(
-                DateTime::parse_from_rfc3339(&first_migration.executed_at).map(|dt| dt.timestamp())
-                    == Ok(now.timestamp())
-            );
+            ensure!(now_timestamp_range.contains(
+                &DateTime::parse_from_rfc3339(&first_migration.executed_at)
+                    .map(|dt| dt.timestamp())
+                    .context("Cannot parse first migration execution date")?
+            ));
 
             let second_migration = migrations_applied
                 .get(1)
                 .context("Cannot get second migration")?;
 
             ensure!(second_migration.script_name == format!("{}02_AddPost", date_prefix));
-            ensure!(
-                DateTime::parse_from_rfc3339(&second_migration.executed_at)
+            ensure!(now_timestamp_range.contains(
+                &DateTime::parse_from_rfc3339(&second_migration.executed_at)
                     .map(|dt| dt.timestamp())
-                    == Ok(now.timestamp())
-            );
+                    .context("Cannot parse second migration execution date")?
+            ));
 
             let third_migration = migrations_applied
                 .get(2)
                 .context("Cannot get third migration")?;
 
             ensure!(third_migration.script_name == format!("{}03_CommentPost", date_prefix));
-            ensure!(
-                DateTime::parse_from_rfc3339(&third_migration.executed_at).map(|dt| dt.timestamp())
-                    == Ok(now.timestamp())
-            );
+            ensure!(now_timestamp_range.contains(
+                &DateTime::parse_from_rfc3339(&third_migration.executed_at)
+                    .map(|dt| dt.timestamp())
+                    .context("Cannot parse third migration execution date")?
+            ));
 
             Ok(())
         })
