bf5a18b93010540553c7c7e3e048aad887069f49
Odonno <bottiau.david@laposte.net>
2023-05-21 23:31:49 +0200

    ♻️ create functions in remove module

--- src/remove.rs
+++ src/remove.rs
@@ -23,46 +23,11 @@ pub fn main() -> Result<()> {
         .last()
         .context("Cannot get last migration")?;
 
-    let last_migration_display_name = last_migration
-        .name
-        .split("_")
-        .skip(2)
-        .map(|s| s.to_string())
-        .collect::<Vec<_>>()
-        .join("_");
+    remove_migration_file(last_migration)?;
+    remove_definition_file_if_exists(last_migration)?;
+    remove_down_migration_file_if_exists(last_migration)?;
 
-    let folder_path = config::retrieve_folder_path();
-    let migrations_path = io::concat_path(&folder_path, MIGRATIONS_DIR_NAME);
-
-    // Remove migration file
-    let migration_file = migrations_path.join(&last_migration.full_name);
-    std::fs::remove_file(migration_file)?;
-
-    // Remove definition file if exists
-    let migration_definition_file_path = Path::new(&migrations_path)
-        .join("definitions")
-        .join(format!("{}.json", last_migration.name));
-
-    if migration_definition_file_path.exists() {
-        std::fs::remove_file(migration_definition_file_path)?;
-    }
-
-    // Remove down migration file if exists
-    let down_migration_file_path = Path::new(&migrations_path)
-        .join(DOWN_MIGRATIONS_DIR_NAME)
-        .join(&last_migration.full_name);
-
-    if down_migration_file_path.exists() {
-        std::fs::remove_file(down_migration_file_path)?;
-    }
-
-    // Remove inlined down migration file if exists
-    let inlined_down_migration_file_path =
-        Path::new(&migrations_path).join(format!("{}.down.surql", last_migration.name));
-
-    if inlined_down_migration_file_path.exists() {
-        std::fs::remove_file(inlined_down_migration_file_path)?;
-    }
+    let last_migration_display_name = get_migration_display_name(last_migration);
 
     println!(
         "Migration '{}' successfully removed",
@@ -77,4 +42,76 @@ fn get_sorted_migrations_files(migrations_files: Vec<SurqlFile>) -> Vec<SurqlFil
     sorted_migrations_files.sort_by(|a, b| a.name.cmp(&b.name));
 
     sorted_migrations_files
+}
+
+fn remove_migration_file(last_migration: &SurqlFile) -> Result<()> {
+    let folder_path = config::retrieve_folder_path();
+    let migrations_path = io::concat_path(&folder_path, MIGRATIONS_DIR_NAME);
+
+    let file_path = migrations_path.join(&last_migration.full_name);
+
+    std::fs::remove_file(file_path)?;
+
+    Ok(())
+}
+
+fn remove_definition_file_if_exists(last_migration: &SurqlFile) -> Result<()> {
+    let folder_path = config::retrieve_folder_path();
+    let migrations_path = io::concat_path(&folder_path, MIGRATIONS_DIR_NAME);
+
+    let migration_definition_file_path = Path::new(&migrations_path)
+        .join("definitions")
+        .join(format!("{}.json", last_migration.name));
+
+    if migration_definition_file_path.exists() {
+        std::fs::remove_file(migration_definition_file_path)?;
+    }
+
+    Ok(())
+}
+
+fn remove_down_migration_file_if_exists(last_migration: &SurqlFile) -> Result<()> {
+    remove_nested_down_migration_file_if_exists(last_migration)?;
+    remove_inlined_down_migration_file_if_exists(last_migration)?;
+
+    Ok(())
+}
+
+fn remove_nested_down_migration_file_if_exists(last_migration: &SurqlFile) -> Result<()> {
+    let folder_path = config::retrieve_folder_path();
+    let migrations_path = io::concat_path(&folder_path, MIGRATIONS_DIR_NAME);
+
+    let down_migration_file_path = Path::new(&migrations_path)
+        .join(DOWN_MIGRATIONS_DIR_NAME)
+        .join(&last_migration.full_name);
+
+    if down_migration_file_path.exists() {
+        std::fs::remove_file(down_migration_file_path)?;
+    }
+
+    Ok(())
+}
+
+fn remove_inlined_down_migration_file_if_exists(last_migration: &SurqlFile) -> Result<()> {
+    let folder_path = config::retrieve_folder_path();
+    let migrations_path = io::concat_path(&folder_path, MIGRATIONS_DIR_NAME);
+
+    let inlined_down_migration_file_path =
+        Path::new(&migrations_path).join(format!("{}.down.surql", last_migration.name));
+
+    if inlined_down_migration_file_path.exists() {
+        std::fs::remove_file(inlined_down_migration_file_path)?;
+    }
+
+    Ok(())
+}
+
+fn get_migration_display_name(migration_file: &SurqlFile) -> String {
+    migration_file
+        .name
+        .split("_")
+        .skip(2)
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>()
+        .join("_")
 }
