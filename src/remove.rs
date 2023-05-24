ba0e4cb5c54e0d74a1cf205df59b3ee36aeaceee
Rom's <romain.kelifa@gmail.com>
2023-05-24 08:03:02 +0700

    ⏪ revert condition in remove

--- src/remove.rs
+++ src/remove.rs
@@ -38,9 +38,7 @@ fn remove_migration_file(last_migration: &SurqlFile) -> Result<()> {
 
     let file_path = migrations_path.join(&last_migration.full_name);
 
-    if file_path.exists() {
-        std::fs::remove_file(file_path)?;
-    }
+    std::fs::remove_file(file_path)?;
 
     Ok(())
 }
